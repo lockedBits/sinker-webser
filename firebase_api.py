8d748ce1d738264bb2682e73c53805f07f8e2f14
Sinker243 <readingnights@outlook.com>
2025-03-19 16:00:18 +0530

    os

--- firebase_api.py
+++ firebase_api.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import json
+import os
 
 firebase_config = json.loads(os.getenv("FIREBASE_CREDENTIALS"))
 cred = credentials.Certificate(firebase_config)
