83f2561e6245ed2a21f4c848c50ea3f5e89309df
Sinker243 <readingnights@outlook.com>
2025-03-19 15:40:13 +0530

    dadadadadadada

--- firebase_api.py
+++ firebase_api.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("path/to/your/firebase/credentials.json")
+firebase_config = json.loads(os.getenv("FIREBASE_CREDENTIALS"))
+cred = credentials.Certificate(firebase_config)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
