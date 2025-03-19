53db8abf459198f5d46848369254e80e6bc271ea
Sinker243 <readingnights@outlook.com>
2025-03-19 21:30:11 +0530

    cors

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from auth import auth_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # <-- This enables CORS globally
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
 
 if __name__ == "__main__":
