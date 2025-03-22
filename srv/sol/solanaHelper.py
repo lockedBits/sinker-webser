53f70e71a741474eb4339f36d3d34c86bcb9e72e
Sinker243 <readingnights@outlook.com>
2025-03-23 01:42:23 +0530

    soldering

--- srv/sol/solanaHelper.py
+++ srv/sol/solanaHelper.py
@@ -1,21 +1,23 @@
-from solana.keypair import Keypair
-from solana.publickey import PublicKey
+from solders.keypair import Keypair
+from solders.pubkey import Pubkey
+from solders.signature import Signature
+from solders.transaction import Transaction
+from solders.instruction import Instruction
+from solders.message import Message
+from solders.system_program import transfer, TransferParams
 from solana.rpc.api import Client
-from solana.transaction import Transaction
-from solana.system_program import TransferParams, transfer
 from base58 import b58encode, b58decode
 
-# Connect to devnet for testing (can be changed to mainnet later)
 SOLANA_RPC_URL = "https://api.devnet.solana.com"
-solana_client = Client(SOLANA_RPC_URL)
+client = Client(SOLANA_RPC_URL)
 
 
 class SolanaHelper:
     @staticmethod
     def generate_wallet():
         keypair = Keypair()
-        private_key = b58encode(keypair.secret_key).decode()
-        public_key = str(keypair.public_key)
+        private_key = b58encode(bytes(keypair)).decode()
+        public_key = str(keypair.pubkey())
         return {
             "private_key": private_key,
             "public_key": public_key
@@ -24,11 +26,10 @@ class SolanaHelper:
     @staticmethod
     def get_balance(public_key: str):
         try:
-            response = solana_client.get_balance(PublicKey(public_key))
-            if response["result"]:
+            response = client.get_balance(Pubkey.from_string(public_key))
+            if "result" in response and response["result"]:
                 lamports = response["result"]["value"]
-                sol = lamports / 1_000_000_000
-                return {"success": True, "balance_sol": sol}
+                return {"success": True, "balance_sol": lamports / 1_000_000_000}
             return {"success": False, "error": "No result in response"}
         except Exception as e:
             return {"success": False, "error": str(e)}
@@ -36,22 +37,23 @@ class SolanaHelper:
     @staticmethod
     def send_sol(from_private_key: str, to_public_key: str, amount_sol: float):
         try:
-            from_keypair = Keypair.from_secret_key(b58decode(from_private_key))
-            to_pubkey = PublicKey(to_public_key)
+            from_keypair = Keypair.from_bytes(b58decode(from_private_key))
+            to_pubkey = Pubkey.from_string(to_public_key)
             lamports = int(amount_sol * 1_000_000_000)
 
-            txn = Transaction()
-            txn.add(
-                transfer(
-                    TransferParams(
-                        from_pubkey=from_keypair.public_key,
-                        to_pubkey=to_pubkey,
-                        lamports=lamports
-                    )
+            ix = transfer(
+                TransferParams(
+                    from_pubkey=from_keypair.pubkey(),
+                    to_pubkey=to_pubkey,
+                    lamports=lamports
                 )
             )
 
-            response = solana_client.send_transaction(txn, from_keypair)
-            return {"success": True, "signature": response["result"]}
+            blockhash = client.get_latest_blockhash()["result"]["value"]["blockhash"]
+            msg = Message([ix], payer=from_keypair.pubkey(), recent_blockhash=blockhash)
+            txn = Transaction(msg, [from_keypair])
+            txn_sig = client.send_transaction(txn)["result"]
+
+            return {"success": True, "signature": txn_sig}
         except Exception as e:
             return {"success": False, "error": str(e)}
